76ba10acb551c3394c3044c17837414a8057aca4
co7ncidence <rodriguezgarcia0987@icloud.com>
2026-05-12 11:54:56 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (_, res) => {
+  res.send("Bot is running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 export { openai, generateImageBuffer, editImages } from "./client";

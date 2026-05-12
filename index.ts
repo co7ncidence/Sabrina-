3bc6bf9f8d293e22a173cbde9ccb10d48f37fb8a
co7ncidence <rodriguezgarcia0987@icloud.com>
2026-05-12 12:25:14 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -18,5 +18,11 @@ const client = new Client({
 client.once("ready", () => {
   console.log(`Logged in as ${client.user?.tag}`);
 });
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
 
+  if (message.content === "!ping") {
+    message.reply("pong");
+  }
+});
 client.login(process.env.DISCORD_TOKEN);

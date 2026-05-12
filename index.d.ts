69a16dedb924c3fcbb698aabf3f9f7ec57e4752c
co7ncidence <rodriguezgarcia0987@icloud.com>
2026-05-12 07:30:10 -0700

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,2 @@
-export { openai } from "./client";
-export { generateImageBuffer, editImages } from "./image";
-export { batchProcess, batchProcessWithSSE, isRateLimitError, type BatchOptions } from "./batch";
+export { openai, generateImageBuffer, editImages } from "./client";
 //# sourceMappingURL=index.d.ts.map
